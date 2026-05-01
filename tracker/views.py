9cd368d141f66b45a46a085fe37de1d8fe081ca1
manyatagupta <manyatagupta1555@gmail.com>
2026-05-01 21:03:40 +0530

    Added Delete button and better UI

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        task_title = request.POST.get('title')
+        if task_title:
+            Task.objects.create(title=task_title)
+            return redirect('index')
+
+    if request.GET.get('complete'):
+        task_id = request.GET.get('complete')
+        task = Task.objects.get(id=task_id)
+        task.is_completed = True
+        task.save()
+        return redirect('index')
+
+    # NAYA DELETE LOGIC
+    if request.GET.get('delete'):
+        task_id = request.GET.get('delete')
+        Task.objects.filter(id=task_id).delete()
+        return redirect('index')
+
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tracker/index.html', {'tasks': tasks})
