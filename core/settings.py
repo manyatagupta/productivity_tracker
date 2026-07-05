50f7d9e4c7b74b7e0f05e31621211bdd285c8c7b
manyatagupta <manyatagupta1555@gmail.com>
2026-07-05 17:10:32 +0530

    config: configure authentication settings and URLs

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
