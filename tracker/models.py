c48802a2ffa893829f0e8c7c9707b2f43af65194
manyatagupta <manyatagupta1555@gmail.com>
2026-07-05 17:10:21 +0530

    feat: add user field to Task model

--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255)
     priority = models.CharField(max_length=10, default='medium')
     is_completed = models.BooleanField(default=False)
